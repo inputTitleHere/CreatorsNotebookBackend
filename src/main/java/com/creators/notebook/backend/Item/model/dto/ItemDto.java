417f673dea757342ab5f2a2c2374219668696145
inputTitleHere <seanbryan97@naver.com>
2023-02-07 23:10:17 +0900

    hypersistence utils for hibernate 5.6(spring boot 2.7.6)을 위한 jackson-module-jaxb-annotations:2.13.4 디펜던시 추가 및 ItemEntity에 대해 JsonB 컬럼 생성

--- src/main/java/com/creators/notebook/backend/Item/model/dto/ItemDto.java
+++ src/main/java/com/creators/notebook/backend/Item/model/dto/ItemDto.java
@@ -1,28 +1,4 @@
 package com.creators.notebook.backend.Item.model.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.DynamicUpdate;
-
-import javax.persistence.*;
-import java.util.UUID;
-
-@Builder
-@Entity
-@Getter
-@Setter
-@Table(name="item")
-@DynamicUpdate
-@DynamicInsert
 public class ItemDto {
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "item_uuid", columnDefinition = "uuid")
-  private UUID itemUuid;
-
-
-
-
 }
--- src/main/java/com/creators/notebook/backend/Item/model/dto/ItemEntity.java
+++ src/main/java/com/creators/notebook/backend/Item/model/dto/ItemEntity.java
@@ -1,4 +1,36 @@
 package com.creators.notebook.backend.Item.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+import java.util.Map;
+import java.util.UUID;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+@Table(name="item")
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class ItemEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "item_uuid", columnDefinition = "uuid")
+  private UUID itemUuid;
+
+  @Type(type = "json")
+  @Column(name = "item_data", columnDefinition = "jsonb")
+  private Map<String ,Object> itemData;
+
+
 }
